e47135a9f15408acd4b6544b318de5bbcbb51b69
Administrator <mohamedmumtaj6@gmail.com>
2025-05-03 19:56:55 +0530

    Module 5: REST APIs created for Student and Teacher

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Student, Teacher
+from .serializers import StudentSerializer, TeacherSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer

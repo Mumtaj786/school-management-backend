9133a046df0d9a47d69e37e4988e710ccb8fef99
Administrator <mohamedmumtaj6@gmail.com>
2025-05-03 19:40:45 +0530

    Module 3: Added Student and Teacher models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_number = models.CharField(max_length=20, unique=True)
+    email = models.EmailField()
+    class_name = models.CharField(max_length=50)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    email = models.EmailField()
